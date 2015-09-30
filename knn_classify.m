ce33bcae5ea436c66a512693e86ca432eb808d40
ruhigit <ruhiagarwal@live.com>
2015-09-30 15:56:16 -0700

    classification algos

--- knn_classify.m
+++ knn_classify.m
@@ -15,3 +15,80 @@ function [new_accu, train_accu] = knn_classify(train_data, train_label, new_data
 %  strategy)
 %
 % CSCI 567: Machine Learning, Fall 2015, Homework 1
+
+size_train_data=size(train_data,1);
+size_test_data=size(new_data,1);
+size_features=size(train_data,2);
+mean=zeros(1,size_features);
+%Standardize data
+%Mean
+for i=1:size_features
+    mean(1,i)=sum(train_data(:,i))/size_train_data;
+end
+std_dev=zeros(1,size_features);
+%Standard Deviation
+for i=1:size_features
+    std_dev(1,i)=sqrt(sum((mean(1,i)-train_data(i,:)).^2)/(size_train_data-1));
+end
+for i=1:size_features
+    for j=1:size_train_data
+        train_data(j,i)=(train_data(j,i)-mean(1,i))/std_dev(1,i);
+    end
+end
+for i=1:size_features
+    for j=1:size_test_data
+        new_data(j,i)=(new_data(j,i)-mean(1,i))/std_dev(1,i);
+    end
+end
+%train_accu=knn(train_data,train_label,train_data,train_label,K,1);
+new_accu=knn(train_data,train_label,new_data,new_label,K,0);
+train_accu=1;
+
+
+function[accuracy]=knn(x,y,x1,y1,k,leave_one_out)
+size_train_data=size(x,1);
+size_test_data=size(x1,1);
+correct=0;
+for i=1:size_test_data
+    count_label_pos=0;
+    count_label_neg=0;
+    dist=zeros(size_train_data,1);
+    for j=1:size_train_data
+        dist(j)=sqrt(sum((x(j,:)-x1(i,:))));
+    end
+    [sorted,indices]=sort(dist);
+    for d=1:k
+        if(y(indices(d))==1)
+            count_label_pos=1+count_label_pos;
+        else
+            count_label_neg=count_label_neg+1;
+        end
+    end
+    if(count_label_pos>count_label_neg)
+        predicted=1;
+    else
+        predicted=2;
+    end
+    if(predicted==y1(i))
+        correct=correct+1;
+    end
+end
+accuracy=(correct)/size_test_data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- naive_bayes.m
+++ naive_bayes.m
@@ -13,3 +13,58 @@ function [new_accu, train_accu] = naive_bayes(train_data, train_label, new_data,
 %  train_accu: accuracy of classifying train_data 
 %
 % CSCI 567: Machine Learning, Fall 2015, Homework 1
+
+%The total no of examples:
+size_dataset=length(train_label);
+total_features=size(train_data,2);
+
+%Calculate the priors for each class label
+[frequency,label]=hist(train_label,unique(train_label)); %Label has the classes and corresponding frequency count of each class in dataset
+total_classes=size(label,1);                       %Total no of classes
+prior = frequency/size_dataset;                         %probability of each class
+
+model=zeros(total_classes,total_features);
+%Count the number of times a feature shows up for each class
+for i=1:total_classes
+    class=label(i);                                    %For each class
+    for j=1:total_features                             %For each feature 
+        k=find(train_label==class);                    %Find all rows that has class i
+        count_feature=sum(train_data(k,j)==1); %for each column j of those selected rows,add the no of 1's
+        model(i,j)=count_feature/frequency(i);
+        if(model(i,j)==0)
+            model(i,j)=0.1;
+        end
+    end 
+end
+new_accu=classify(train_label,model,new_data,new_label);
+train_accu=classify(train_label,model,train_data,train_label);
+
+function[accu]=classify(train_label,model,new_data,new_label)
+size_dataset=length(train_label);
+%Calculate the priors for each class label
+[frequency,label]=hist(train_label,unique(train_label)); %Label has the classes and corresponding frequency count of each class in dataset
+total_classes=size(label,1);                       %Total no of classes
+prior = frequency/size_dataset;                         %probability of each class
+%Classify data
+total_testfeatures=size(new_data,2);
+total_testeg=length(new_label);
+predictedLabels=zeros(total_testeg,1);
+for t=1:total_testeg
+    prob=zeros(1,total_classes);
+    for i=1:total_classes
+        prob_f_given_c=1;
+        for j=1:total_testfeatures
+            if(new_data(t,j)==1)
+                    prob_j=model(i,j);
+                prob_f_given_c=prob_f_given_c*prob_j;
+            end
+        end
+        prob(i)=prior(i)*prob_f_given_c;
+    end
+    %Assign the class with the highest posterior probability
+    [max_prob, class_index] = max(prob);
+    predictedLabels(t) = label(class_index);
+end
+ accu = sum(predictedLabels == new_label)/total_testeg;
+
+
